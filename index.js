08195caaa215fcd9f26d9a33d799ecd5bc535eb7
Shubham <shubham.sharma@extended.highspot.com>
2024-04-03 18:51:57 +0530

    final commit

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const rssGenerator = require('./services/rss.js');
 require('./services/cron.js');
 
 dotenv.config();
@@ -9,24 +8,12 @@ const app = express();
 app.use(express.json());
 
 app.get('/health', (req, res) => {
-    console.log('running');
   const data = {
     message: 'Health API Running Succcessfully',
     code: 200,
   };
 
   res.status(200).send(data);
-});
-
-app.get('/rss', (req, res) => {
-    rssGenerator();
-
-    const data = {
-      message: 'RSS Sent Succcessfully',
-      code: 200,
-    };
-  
-    res.status(200).send(data);
 });
 
 app.listen(process.env.PORT, () => console.log(`Server running on port: ${process.env.PORT || 3000} `));
--- services/rss.js
+++ services/rss.js
@@ -5,14 +5,13 @@ const compareTime = require('../helpers/index.js');
 const parser = new Parser();
 
 const rssGenerator = async () => {
-    console.log(process.env.RSSURL);
     const feed = await parser.parseURL(process.env.RSSURL);
     const items = feed?.items;
 
     items.forEach(async (item) => {
         const minutesDifference = compareTime(item?.pubDate);
         // Check if the difference is less than 5 minutes
-        if (minutesDifference < 60) {
+        if (minutesDifference < 5) {
             console.log('The UTC time is less than 5 minutes old.');
             mailer(item);
         } else {
